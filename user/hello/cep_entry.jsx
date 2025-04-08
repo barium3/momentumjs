562893b234719024d679042d93e3993e67791087
Chenrui Hu <97895452+barium3@users.noreply.github.com>
2025-04-08 11:03:51 +0800

    Delete user directory

--- user/hello/cep_entry.jsx
+++
@@ -1,5 +0,0 @@
-(function () {
-  var extensionRoot = new File($.fileName).parent.parent.fsName;
-  $.evalFile(extensionRoot + "/bundle/momentum.js");
-  $.evalFile(extensionRoot + "/user/非遗.js");
-})();
